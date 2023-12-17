6df01fdf50b75ecfdabba7312839f06ba84eed22
Lyn-Green <prentice9823@gmail.com>
2023-12-17 22:47:51 +0800

    add: banner-animation

--- NiShangGe/js/script.js
+++ NiShangGe/js/script.js
@@ -36,5 +36,27 @@ $(function(){
     });
 })
 
+//index-banner 煙霧效果
+const filter = document.querySelector("#turbulence");
+let frames = 1;
+let rad = Math.PI / 180;
+let bfx, bfy;
+
+function freqAnimation() {
+    frames += .2
+
+    bfx = 0.03;
+    bfy = 0.03;
+
+    bfx += 0.005 * Math.cos(frames * rad);
+    bfy += 0.005 * Math.sin(frames * rad);
+
+    bf = bfx.toString() + " " + bfy.toString();
+    filter.setAttributeNS(null, "baseFrequency", bf);
+
+    window.requestAnimationFrame(freqAnimation);
+}
+
+window.requestAnimationFrame(freqAnimation);
 
 
